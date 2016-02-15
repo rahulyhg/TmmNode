df419fd19f58c4fc3609e710d520f703a752fe4e
vigkast <vigwohlig@gmail.com>
2016-02-15 15:26:02 +0530

    fidn for web

--- api/models/Folder.js
+++ api/models/Folder.js
@@ -1,251 +1,295 @@
 module.exports = {
-  save: function(data, callback) {
-    if (data.image && data.image[0]) {
-      if (data.timestamp && data.timestamp != "") {
-        data.timestamp = new Date();
-      }
-      sails.query(function(err, db) {
-        if (err) {
-          console.log(err);
-          callback({
-            value: false
-          });
-        }
-        if (db) {
-          if (!data._id) {
-            data._id = sails.ObjectID();
-            db.collection('folder').insert(data, function(err, created) {
-              if (err) {
-                console.log(err);
-                callback({
-                  value: false,
-                  comment: "Error"
-                });
-                db.close();
-              } else if (created) {
-                callback({
-                  value: true,
-                  id: data._id
-                });
-                db.close();
-              } else {
-                callback({
-                  value: false,
-                  comment: "Not created"
-                });
-                db.close();
-              }
-            });
-          } else {
-            var folder = sails.ObjectID(data._id);
-            delete data._id
-            db.collection('folder').update({
-              _id: folder
-            }, {
-              $set: data
-            }, function(err, updated) {
-              if (err) {
-                console.log(err);
-                callback({
-                  value: false,
-                  comment: "Error"
-                });
-                db.close();
-              } else if (updated.result.nModified != 0 && updated.result.n != 0) {
-                callback({
-                  value: true
-                });
-                db.close();
-              } else if (updated.result.nModified == 0 && updated.result.n != 0) {
-                callback({
-                  value: true,
-                  comment: "Data already updated"
-                });
-                db.close();
-              } else {
-                callback({
-                  value: false,
-                  comment: "No data found"
-                });
-                db.close();
-              }
-            });
-          }
-        }
-      });
-    } else {
-      callback({
-        value: false,
-        comment: "Please enter correct parameters"
-      });
-    }
-  },
-  find: function(data, callback) {
-    sails.query(function(err, db) {
-      if (err) {
-        console.log(err);
-        callback({
-          value: false
-        });
-      }
-      if (db) {
-        db.collection("folder").find().toArray(function(err, found) {
-          if (err) {
-            callback({
-              value: false
-            });
-            db.close();
-          } else if (found && found[0]) {
-            callback(found);
-            db.close();
-          } else {
-            callback({
-              value: false,
-              comment: "No data found"
-            });
-            db.close();
-          }
-        });
-      }
-    });
-  },
-  //Findlimited
-  findlimited: function(data, callback) {
-    var i = 0;
-    var newreturns = {};
-    newreturns.data = [];
-    var pagesize = parseInt(data.pagesize);
-    var pagenumber = parseInt(data.pagenumber);
-    sails.query(function(err, db) {
-      if (err) {
-        console.log(err);
-        callback({
-          value: false
-        });
-      }
-      if (db) {
-        callbackfunc1();
-
-        function callbackfunc1() {
-          db.collection("folder").count({}, function(err, number) {
-            if (number && number != "") {
-              newreturns.total = number;
-              newreturns.totalpages = Math.ceil(number / data.pagesize);
-              callbackfunc();
-            } else if (err) {
-              console.log(err);
-              callback({
-                value: false
-              });
-              db.close();
-            } else {
-              callback({
-                value: false,
-                comment: "Count of null"
-              });
-              db.close();
+    save: function(data, callback) {
+        if (data.image && data.image[0]) {
+            if (data.timestamp && data.timestamp != "") {
+                data.timestamp = new Date();
             }
-          });
-
-          function callbackfunc() {
-            db.collection("folder").find({}).skip(pagesize * (pagenumber - 1)).limit(pagesize).toArray(function(err, found) {
-              if (err) {
-                callback({
-                  value: false
-                });
-                console.log(err);
-                db.close();
-              } else if (found && found[0]) {
-                _.each(found, function(n) {
-                  n.thumb = sails._.first(n.image);
-                  delete n.image;
-                  i++;
-                  newreturns.data.push(n);
-                  if (i == found.length) {
-                    callback2();
-                  }
-                });
-
-                function callback2() {
-                  callback(newreturns);
-                  db.close();
+            sails.query(function(err, db) {
+                if (err) {
+                    console.log(err);
+                    callback({
+                        value: false
+                    });
                 }
-              } else {
-                callback({
-                  value: false,
-                  comment: "No data found"
-                });
-                db.close();
-              }
+                if (db) {
+                    if (!data._id) {
+                        data._id = sails.ObjectID();
+                        db.collection('folder').insert(data, function(err, created) {
+                            if (err) {
+                                console.log(err);
+                                callback({
+                                    value: false,
+                                    comment: "Error"
+                                });
+                                db.close();
+                            } else if (created) {
+                                callback({
+                                    value: true,
+                                    id: data._id
+                                });
+                                db.close();
+                            } else {
+                                callback({
+                                    value: false,
+                                    comment: "Not created"
+                                });
+                                db.close();
+                            }
+                        });
+                    } else {
+                        var folder = sails.ObjectID(data._id);
+                        delete data._id
+                        db.collection('folder').update({
+                            _id: folder
+                        }, {
+                            $set: data
+                        }, function(err, updated) {
+                            if (err) {
+                                console.log(err);
+                                callback({
+                                    value: false,
+                                    comment: "Error"
+                                });
+                                db.close();
+                            } else if (updated.result.nModified != 0 && updated.result.n != 0) {
+                                callback({
+                                    value: true
+                                });
+                                db.close();
+                            } else if (updated.result.nModified == 0 && updated.result.n != 0) {
+                                callback({
+                                    value: true,
+                                    comment: "Data already updated"
+                                });
+                                db.close();
+                            } else {
+                                callback({
+                                    value: false,
+                                    comment: "No data found"
+                                });
+                                db.close();
+                            }
+                        });
+                    }
+                }
             });
-          }
-        }
-      }
-    });
-  },
-  //Findlimited
-  findone: function(data, callback) {
-    sails.query(function(err, db) {
-      if (err) {
-        console.log(err);
-        callback({
-          value: false
-        });
-      }
-      if (db) {
-        db.collection("folder").find({
-          _id: sails.ObjectID(data._id)
-        }).toArray(function(err, data2) {
-          if (err) {
-            console.log(err);
-            callback({
-              value: false
-            });
-            db.close();
-          } else if (data2 && data2[0]) {
-            callback(data2[0]);
-            db.close();
-          } else {
-            callback({
-              value: false,
-              comment: "No data found"
-            });
-            db.close();
-          }
-        });
-      }
-    });
-  },
-  delete: function(data, callback) {
-    sails.query(function(err, db) {
-      if (err) {
-        console.log(err);
-        callback({
-          value: false
-        });
-      }
-      db.collection('folder').remove({
-        _id: sails.ObjectID(data._id)
-      }, function(err, deleted) {
-        if (deleted) {
-          callback({
-            value: true
-          });
-          db.close();
-        } else if (err) {
-          console.log(err);
-          callback({
-            value: false
-          });
-          db.close();
         } else {
-          callback({
-            value: false,
-            comment: "No data found"
-          });
-          db.close();
+            callback({
+                value: false,
+                comment: "Please enter correct parameters"
+            });
         }
-      });
-    });
-  }
+    },
+    find: function(data, callback) {
+        sails.query(function(err, db) {
+            if (err) {
+                console.log(err);
+                callback({
+                    value: false
+                });
+            }
+            if (db) {
+                db.collection("folder").find().toArray(function(err, found) {
+                    if (err) {
+                        callback({
+                            value: false
+                        });
+                        db.close();
+                    } else if (found && found[0]) {
+                        callback(found);
+                        db.close();
+                    } else {
+                        callback({
+                            value: false,
+                            comment: "No data found"
+                        });
+                        db.close();
+                    }
+                });
+            }
+        });
+    },
+    findweb: function(data, callback) {
+        sails.query(function(err, db) {
+            if (err) {
+                console.log(err);
+                callback({
+                    value: false
+                });
+            }
+            if (db) {
+                db.collection("folder").aggregate([{
+                    $unwind: "$image"
+                }, {
+                    $group: {
+                        _id: null,
+                        images: {
+                            $addToSet: "$image"
+                        }
+                    }
+                }, {
+                    $project: {
+                        _id: 0,
+                        images: 1
+                    }
+                }]).toArray(function(err, found) {
+                    if (err) {
+                        console.log(err);
+                        callback({
+                            value: false
+                        });
+                        db.close();
+                    } else if (found && found[0]) {
+                        callback(found[0]);
+                        db.close();
+                    } else {
+                        callback({
+                            value: false,
+                            comment: "No data found"
+                        });
+                        db.close();
+                    }
+                });
+            }
+        });
+    },
+    //Findlimited
+    findlimited: function(data, callback) {
+        var i = 0;
+        var newreturns = {};
+        newreturns.data = [];
+        var pagesize = parseInt(data.pagesize);
+        var pagenumber = parseInt(data.pagenumber);
+        sails.query(function(err, db) {
+            if (err) {
+                console.log(err);
+                callback({
+                    value: false
+                });
+            }
+            if (db) {
+                callbackfunc1();
+
+                function callbackfunc1() {
+                    db.collection("folder").count({}, function(err, number) {
+                        if (number && number != "") {
+                            newreturns.total = number;
+                            newreturns.totalpages = Math.ceil(number / data.pagesize);
+                            callbackfunc();
+                        } else if (err) {
+                            console.log(err);
+                            callback({
+                                value: false
+                            });
+                            db.close();
+                        } else {
+                            callback({
+                                value: false,
+                                comment: "Count of null"
+                            });
+                            db.close();
+                        }
+                    });
+
+                    function callbackfunc() {
+                        db.collection("folder").find({}).skip(pagesize * (pagenumber - 1)).limit(pagesize).toArray(function(err, found) {
+                            if (err) {
+                                callback({
+                                    value: false
+                                });
+                                console.log(err);
+                                db.close();
+                            } else if (found && found[0]) {
+                                _.each(found, function(n) {
+                                    n.thumb = sails._.first(n.image);
+                                    delete n.image;
+                                    i++;
+                                    newreturns.data.push(n);
+                                    if (i == found.length) {
+                                        callback2();
+                                    }
+                                });
+
+                                function callback2() {
+                                    callback(newreturns);
+                                    db.close();
+                                }
+                            } else {
+                                callback({
+                                    value: false,
+                                    comment: "No data found"
+                                });
+                                db.close();
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    },
+    //Findlimited
+    findone: function(data, callback) {
+        sails.query(function(err, db) {
+            if (err) {
+                console.log(err);
+                callback({
+                    value: false
+                });
+            }
+            if (db) {
+                db.collection("folder").find({
+                    _id: sails.ObjectID(data._id)
+                }).toArray(function(err, data2) {
+                    if (err) {
+                        console.log(err);
+                        callback({
+                            value: false
+                        });
+                        db.close();
+                    } else if (data2 && data2[0]) {
+                        callback(data2[0]);
+                        db.close();
+                    } else {
+                        callback({
+                            value: false,
+                            comment: "No data found"
+                        });
+                        db.close();
+                    }
+                });
+            }
+        });
+    },
+    delete: function(data, callback) {
+        sails.query(function(err, db) {
+            if (err) {
+                console.log(err);
+                callback({
+                    value: false
+                });
+            }
+            db.collection('folder').remove({
+                _id: sails.ObjectID(data._id)
+            }, function(err, deleted) {
+                if (deleted) {
+                    callback({
+                        value: true
+                    });
+                    db.close();
+                } else if (err) {
+                    console.log(err);
+                    callback({
+                        value: false
+                    });
+                    db.close();
+                } else {
+                    callback({
+                        value: false,
+                        comment: "No data found"
+                    });
+                    db.close();
+                }
+            });
+        });
+    }
 };
